17029f41e797496ead32f728fbde3e899e27fc33
fasterbetter <malloctime@163.com>
2023-12-27 14:36:33 +0800

    first

---
+++ src/main/java/base/demo1/Demo1.java
@@ -0,0 +1,12 @@
+package base.demo1;
+
+/**
+ * @author limingqiang
+ * @version 1.0
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
